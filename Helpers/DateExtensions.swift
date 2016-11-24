17994be202232f77376749764017f91e86cd9ef5
Sebastián Varela <sebastian.varela@s3ba.net>
2016-11-24 12:58:22 +0100

    Improve date formatter

--- Helpers/DateExtensions.swift
+++ Helpers/DateExtensions.swift
@@ -8,6 +8,22 @@ public extension Date {
         let converted = dateFormatter.string(from: self)
         return converted
     }
+    
+    public func toFormattedSpanishFullDateString() -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "dd/MM/yyyy - HH:mm"
+        let converted = dateFormatter.string(from: self)
+        return converted
+    }
+    
+    public func toFormattedSpanishFullDateString(withEndDate end: Date) -> String {
+        let datef = DateFormatter()
+        datef.dateFormat = "dd/MM/yyyy"
+        let timef = DateFormatter()
+        timef.dateFormat = "HH:mm"
+        
+        return "\(datef.string(from: self)) - \(timef.string(from: self)) a \(timef.string(from: end))"
+    }
 
     public func toFormattedJsonDateString() -> String {
         let dateFormatter = DateFormatter()
---
+++ Test.playground/Contents.swift
@@ -0,0 +1,12 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+import Helpers
+
+let a = Date()
+a.toFormattedSpanishFullDateString(withEndDate: Date() + 300)
+
+let bb = ""
+bb.toDateFromJsonFormat()
+
+let aa = (Date(), Date())
