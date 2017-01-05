1ee07d8f6baad2289614a572ed9e8dae2f1bd6b0
Sebastián Varela <sebastian.varela@s3ba.net>
2017-01-05 23:51:37 +0100

    Add UIView Extensions to align views

---
+++ Helpers/UIViewExtensions.swift
@@ -0,0 +1,16 @@
+
+import UIKit
+import Foundation
+
+public extension UIView {
+    public func alignToView(_ view: UIView) {
+        addConstraintToView(view, attribute: .top, constant: 0)
+        addConstraintToView(view, attribute: .bottom, constant: 0)
+        addConstraintToView(view, attribute: .leading, constant: 0)
+        addConstraintToView(view, attribute: .trailing, constant: 0)
+    }
+    
+    private func addConstraintToView(_ view: UIView, attribute: NSLayoutAttribute, constant: CGFloat) {
+        addConstraint(NSLayoutConstraint(item: view, attribute: attribute, relatedBy: .equal, toItem: self, attribute: attribute, multiplier: 1.0, constant: constant))
+    }
+}
