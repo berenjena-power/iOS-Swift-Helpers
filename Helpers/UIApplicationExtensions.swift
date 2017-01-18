b28f035c7ca4981b4ca11de337eaa2e2d1de6ea4
Sebastián Varela <sebastian.varela@s3ba.net>
2017-01-18 14:40:34 +0100

    fix

--- Helpers/UIApplicationExtensions.swift
+++ Helpers/UIApplicationExtensions.swift
@@ -2,7 +2,7 @@ import Foundation
 
 public extension UIApplication {
     public var statusBar: UIView {
-        let statusBar = UIApplication.shared.value(forKey: "statusBar") as! UIView
+        let statusBar = value(forKey: "statusBar") as! UIView
         return statusBar
     }
 }
