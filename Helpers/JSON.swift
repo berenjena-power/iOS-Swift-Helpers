beed33183309d4f3c5cadee7d64fa3be32a1645e
Sebastián Varela <sebastian.varela@s3ba.net>
2017-01-24 17:16:39 +0100

    Add extension for JSON (aka Dictionary of String: Any)

--- Helpers/JSON.swift
+++ Helpers/JSON.swift
@@ -1,3 +1,13 @@
 import Foundation
 
 public typealias JSON = [String : Any]
+
+public extension Dictionary where Key: ExpressibleByStringLiteral, Value: Any {
+    public func toJSONString() -> String? {
+        guard let data = try? JSONSerialization.data(withJSONObject: self, options: []),
+            let json = NSString(data: data as Data, encoding: String.Encoding.utf8.rawValue) else {
+                return nil
+        }
+        return json as String
+    }
+}
