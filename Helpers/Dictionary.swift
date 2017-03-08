f51e1ebe46a60f756ad53b4ff1754f4dd1c9203e
Sebastián Varela <sebastian.varela@s3ba.net>
2017-03-08 11:58:22 +0100

    resolve minor warning

--- Helpers/Dictionary.swift
+++ Helpers/Dictionary.swift
@@ -2,8 +2,8 @@ import Foundation
 
 extension Dictionary {
     mutating func update(other: Dictionary) {
-        for (key,value) in other {
-            self.updateValue(value, forKey:key)
+        for (key, value) in other {
+            self.updateValue(value, forKey: key)
         }
     }
 }
