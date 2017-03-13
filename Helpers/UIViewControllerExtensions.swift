ca872242fdc39413eb36be0b2567c4b09949a0fa
Sebastián Varela <sebastian.varela@s3ba.net>
2017-03-13 17:40:03 +0100

    Add VC Extension for init from storyboard

--- Helpers/UIViewControllerExtensions.swift
+++ Helpers/UIViewControllerExtensions.swift
@@ -18,4 +18,13 @@ public extension UIViewController {
         }
         return self
     }
+
+    public static func initFromStoryboard<T>(name: String, identifier: String) -> T {
+        guard let vc = UIStoryboard(name: name, bundle: nil)
+            .instantiateViewController(withIdentifier: identifier) as? T else {
+                fatalError("\(identifier) could not be loaded from \(name) as \(T.self)")
+        }
+        
+        return vc
+    }
 }
