d554e64e09c3ef0f554d2dfc4884cecc0f75122f
Sebastián Varela <sebastian.varela@s3ba.net>
2017-03-13 17:40:42 +0100

    Add generic extensions for Array to work with random items

--- Helpers/ArrayExtensions.swift
+++ Helpers/ArrayExtensions.swift
@@ -1,6 +1,45 @@
 import Foundation
 
+public extension Sequence {
+    func group<U: Hashable>(by key: (Iterator.Element) -> U) -> [U:[Iterator.Element]] {
+        var categories: [U: [Iterator.Element]] = [:]
+        for element in self {
+            let key = key(element)
+            if case nil = categories[key]?.append(element) {
+                categories[key] = [element]
+            }
+        }
+        return categories
+    }
+}
+
 public extension Array {
+    /// Returns an array containing this sequence shuffled
+    public var shuffled: Array {
+        var elements = self
+        return elements.shuffle()
+    }
+    
+    /// Shuffles this sequence in place
+    @discardableResult
+    public mutating func shuffle() -> Array {
+        indices.dropLast().forEach {
+            guard case let index = Int(arc4random_uniform(UInt32(count - $0))) + $0, index != $0 else {
+                return
+            }
+            swap(&self[$0], &self[index])
+        }
+        return self
+    }
+    
+    public var randomItem: Element {
+        return self[Int(arc4random_uniform(UInt32(count)))]
+    }
+    
+    public func randomItems(maxItems: Int) -> Array {
+        return Array(shuffled.prefix(maxItems))
+    }
+    
     public func mapOptionals<U>(_ transform: (Element) -> U?) -> [U]? {
         var result: [U] = []
         
