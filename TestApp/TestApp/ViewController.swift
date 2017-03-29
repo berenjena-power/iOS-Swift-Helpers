14d7fe5f7aefef3a92fe63860601ffd1b0e4cf4f
Sebastián Varela <sebastian.varela@s3ba.net>
2017-03-29 13:48:06 +0200

    Add empty app for testing

---
+++ TestApp/TestApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestApp
+//
+//  Created by Sebastián Varela on 29/3/17.
+//  Copyright © 2017 sebastianvarela. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
