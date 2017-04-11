7ea25e1c6922a6414b56869a499a64cba3be419b
José María Puerta <jose.maria.puerta@ciber.com>
2017-04-11 11:29:45 +0200

    Move request to Hermes

--- Helpers/Helpers/Dictionary.swift
+++ Helpers/Helpers/Dictionary.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 extension Dictionary {
-    mutating func update(other: Dictionary) {
+    public mutating func update(other: Dictionary) {
         for (key, value) in other {
             self.updateValue(value, forKey: key)
         }
