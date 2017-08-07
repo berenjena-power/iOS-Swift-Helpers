5777cc033e99fa2eead03799de62d0497d676742
Sebastián Varela <sebastian.varela@s3ba.net>
2017-08-07 13:30:39 +0200

    add global delay function

--- Helpers/Helpers/Utils.swift
+++ Helpers/Helpers/Utils.swift
@@ -3,3 +3,7 @@ import Foundation
 public func NSLocalizedString(_ key: String) -> String {
     return NSLocalizedString(key, comment: "")
 }
+
+public func delay(_ delay: Double, closure: @escaping () -> Void) {
+    DispatchQueue.main.asyncAfter(deadline: .now() + delay, execute: closure)
+}
